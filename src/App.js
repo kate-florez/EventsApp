83b41bda5f7bdcdcb3ef8d831d7fa363715c2919
Kate <catherine@sooryen.com>
2019-03-08 16:49:05 -0500

    Login FE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Account/Login/Login";
 import Events from "./components/Events/Events";
 import Contact from "./components/Contact/Contact";
 
@@ -12,12 +13,14 @@ class Main extends Component {
           <h1>Bday Web App</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/Events">Events</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
+            <li><NavLink to="/events">Events</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
-            <Route path="/Events" component={Events}/>
+            <Route path="/login" component={Login} />
+            <Route path="/events" component={Events}/>
             <Route path="/contact" component={Contact}/>
           </div>
         </div>
