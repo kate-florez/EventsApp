8a2d7d72687b31d43f584a9e0a589f29e7987a02
jschles-sooryen <john.schlesinger@sooryen.com>
2019-04-16 17:30:24 -0400

    Implement logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,8 @@ import SignOutButton from '../../components/Account/SignOut/SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../../components/Session';
 
-import "./Navigation.css";
+import './Navigation.css';
+import logo from '../../images/Logo Shape.png';
 
 class Navigation extends Component {
   constructor(props) {
@@ -36,32 +37,35 @@ class Navigation extends Component {
 
 const NavigationAuth = (props) => (
   <div className="main-nav">
-    <ul className="header">
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.HOME}>Home</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.SIGN_IN}>Sign In</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.SIGN_UP}>Sign Up</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.ACCOUNT}>Account</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.ADMIN}>Admin</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.EVENTS}>Events</NavLink>
-      </li>
-      <li>
-        <NavLink onClick={props.onNavClick} to={ROUTES.CONTACT}>Contact</NavLink>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
+    <div className="upper">
+      <img className="logo" src={logo} alt="Events" />
+      <ul className="header">
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.HOME}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.SIGN_IN}>Sign In</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.SIGN_UP}>Sign Up</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.ACCOUNT}>Account</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.ADMIN}>Admin</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.EVENTS}>Events</NavLink>
+        </li>
+        <li>
+          <NavLink onClick={props.onNavClick} to={ROUTES.CONTACT}>Contact</NavLink>
+        </li>
+        <li>
+          <SignOutButton />
+        </li>
+      </ul>
+    </div>
     <h1 className="current-page">{props.page}</h1>
   </div>
 );
